c0f07af4cd1782005a47bb3ef962b21ff5f8168d
Krishna Kumar <getkrishnakumar1990@gmail.com>
2018-08-30 22:34:59 +0530

    Initial commit

---
+++ backend/routes/api/user.js
@@ -0,0 +1,12 @@
+const express  = require('express');
+const userRoute = express.Router();
+
+userRoute.get('/login',(req,res,next)=>{
+    
+});
+
+userRoute.post('/register',(req,res,next)=>{
+
+})
+
+module.exports = userRoute;
